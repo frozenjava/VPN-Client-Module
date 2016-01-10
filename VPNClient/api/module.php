7c34d51940fe8821196a0d43b5a301ecd61f5989
frozenjava <newbi3@innoc-sec.net>
2016-01-10 11:23:57 -0500

    Started working on api

--- VPNClient/api/module.php
+++ VPNClient/api/module.php
@@ -6,8 +6,15 @@ class VPNClient extends Module
 	public function route()
 	{
 		switch ($this->request) {
-			
+			case "checkDependencies":
+				break;
 		}
+	}
+
+	private function checkDependencies()
+	{
+		$openVPN = ($this->checkDependency("openvpn-openssl") && $this->checkDependency("openvpn-easy-rsa"));
+		$pptpVPN = false;
 	}
 
 }
