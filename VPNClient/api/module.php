13d1b07987cfd604a93d8c7f9dc3a8a25125c451
newbi3 <josh@frozendevelopment.net>
2016-01-10 18:36:03 -0500

    More work

--- VPNClient/api/module.php
+++ VPNClient/api/module.php
@@ -5,16 +5,93 @@ class VPNClient extends Module
 
 	public function route()
 	{
-		switch ($this->request) {
-			case "checkDependencies":
+		switch ($this->request->action) {
+			case "getControls":
+				$this->getControls();
+				break;
+
+			case "handleOpenVPNDepends":
+				$this->handleOpenVPNDepends();
+				break;
+
+			case "handlePPTPDepends":
+				$this->handlePPTPDepends();
 				break;
 		}
 	}
 
-	private function checkDependencies()
+	private function getControls()
 	{
 		$openVPN = ($this->checkDependency("openvpn-openssl") && $this->checkDependency("openvpn-easy-rsa"));
-		$pptpVPN = false;
+		$pptpVPN = $this->checkDependency("ppp-mod-pptp");
+
+		$this->response = array("openvpn" => $openVPN, "pptp" => $pptpVPN);
+	}
+
+	private function handleOpenVPNDepends()
+	{
+		$response_array = array();
+		$installed = ($this->checkDependency("openvpn-openssl") && $this->checkDependency("openvpn-easy-rsa"));
+
+		if (!$installed) {
+			$successOpenSSL = $this->installDependency("openvpn-openssl");
+			$successRSA = $this->installDependency("openvpn-easy-rsa");
+			$success = ($successRSA && $successOpenSSL);
+
+			$message = "Successfully installed dependencies.";
+
+			if (!$successOpenSSL && $successRSA) {
+				$message = "Error occured while installing package 'openvpn-openssl'.";
+			} else if ($successOpenSSL && !$successRSA) {
+				$message = "Error occured while installing package 'openvpn-easy-rsa'.";
+			} else if (!$successOpenSSL && !$successRSA) {
+				$message = "Error occured while install packages 'openvpn-openssl' and 'openvpn-easy-rsa'.";
+			}
+
+			$response_array = array("control_succcess" => $success, "control_message" => $message);
+
+		} else {
+			exec("opkg remove openvpn-openssl openvpn-easy-rsa");
+			$removed = !($this->checkDependency("openvpn-openssl") && $this->checkDependency("openvpn-easy-rsa"));
+			$message = "Successfully removed dependencies.";
+			if (!$removed) {
+				$message = "Error removing dependencies.";
+			}
+			$response_array = array("control_succcess" => $removed, "control_message" => $message);
+		}
+
+		$this->response = $response_array;
+
+	}
+
+	private function handlePPTPDepends()
+	{
+		$response_array = array();
+		$installed = $this->checkDependency("ppp-mod-pptp");
+
+		if (!$installed) {
+			$success = $this->installDependency("ppp-mod-pptp");
+
+			$message = "Successfully installed dependencies.";
+
+			if (!$success) {
+				$message = "Error occured while installing 'ppp-mod-pptp'";
+			}
+
+			$response_array = array("control_succcess" => $success, "control_message" => $message);
+
+		} else {
+			exec("opkg remove ppp-mod-pptp");
+			$removed = !$this->checkDependency("ppp-mod-pptp");
+			$message = "Successfully removed dependencies.";
+			if (!$removed) {
+				$message = "Error removing dependencies.";
+			}
+			$response_array = array("control_succcess" => $removed, "control_message" => $message);
+		}
+
+		$this->response = $response_array;
+
 	}
 
 }
--- VPNClient/js/module.js
+++ VPNClient/js/module.js
@@ -1,21 +1,12 @@
 registerController("VPNClientController", ['$api', '$scope', function($api, $scope) {
 
-	$scope.messages = [];
+	getControls();
 
-	$scope.controls = [
-		{
-			title: "OpenVPN",
-			status: "Install",
-			visible: true,
-			throbber: false
-		},
-		{
-			title: "PPTP VPN",
-			status: "Install",
-			visible: true,
-			throbber: false
-		}
-	];
+	$scope.messages = [];
+	$scope.throbber = false;
+	$scope.openVPNInstalled = false;
+	$scope.pptpVPNInstalled = false;
+	$scope.uploadKeys = {};
 
 	$scope.sendMessage = function(t, m) {
 		// Add a new message to the top of the list
@@ -30,6 +21,77 @@ registerController("VPNClientController", ['$api', '$scope', function($api, $sco
 	$scope.dismissMessage = function($index) {
 		//var index = $scope.messages.indexOf(message);
 		$scope.messages.splice($index, 1);
+	}
+
+	$scope.handleControl = function(control) {
+		control.throbber = true;
+		switch (control.title) {
+			case "OpenVPN Dependencies":
+				$api.request({
+					module: "VPNClient",
+					action: "handleOpenVPNDepends"
+				}, function(response) {
+					getControls();
+					control.throbber = false;
+					$scope.sendMessage(control.title, response.control_message);
+				});
+				break;
+
+			case "PPTP Dependencies":
+				$api.request({
+					module: "VPNClient",
+					action: "handlePPTPDepends"
+				}, function(response) {
+					getControls();
+					control.throbber = false;
+					$scope.sendMessage(control.title, response.control_message);
+				});
+				break;
+		}
+	}
+
+	function getControls() {
+		$scope.throbber = true;
+		$api.request({
+			module: "VPNClient",
+			action: "getControls"
+		}, function(response) {
+			console.log(response);
+			updateControls(response);
+		});
+	}
+
+	function updateControls(response) {
+		var openVPNStatus = "Install";
+		var pptpStatus = "Install";
+		$scope.openVPNInstalled = false;
+		$scope.pptpVPNInstalled = false;
+
+		if (response.openvpn) {
+			openVPNStatus = "Uninstall";
+			$scope.openVPNInstalled = true;
+		}
+
+		if (response.pptp) {
+			pptpStatus = "Uninstall";
+			$scope.pptpVPNInstalled = true;
+		}
+
+		$scope.controls = [
+			{
+				title: "OpenVPN Dependencies",
+				status: openVPNStatus,
+				visible: true,
+				throbber: false
+			},
+			{
+				title: "PPTP Dependencies",
+				status: pptpStatus,
+				visible: true,
+				throbber: false
+			}
+		];
+		$scope.throbber = false;
 	}
 
 }]);
